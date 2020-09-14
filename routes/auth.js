ebf0ab4571ec8d3537d0436509f035b5ed633ab4
Habid B Espinosa <hab.bad@hotmail.com>
2020-09-14 15:44:30 +0200

    the new routes for login and conditions for sign up are added

--- routes/auth.js
+++ routes/auth.js
@@ -9,9 +9,40 @@ router.get('/signup', (req, res, next) => {
   res.render('auth/signup');
 });
 
+router.get('/login', (req, res, next) => {
+  res.render('auth/login');
+});
 
+router.post('/signup', (req, res, next) => {
 
+  const { username, password } = req.body;
 
+  if (password.length < 8) {
+    res.render('auth/signup', { message: 'Your password needs to be 8 chars min' });
+    return;
+  }
+  if (username === '') {
+    res.render('auth/signup', { message: 'Your username cannot be empty' });
+    return;
+  }
+  User.findOne({ username: username })
+    .then(found => {
+      if (found !== null) {
+        res.render('auth/signup', { message: 'This username is already taken' });
+      } else {
+        const salt = bcrypt.genSaltSync();
+        const hash = bcrypt.hashSync(password, salt);
+
+        User.create({
+          username: username,
+          password: hash
+        })
+          .then(dbUser => {
+            res.redirect('/login');
+          })
+      }
+    })
+})
 
 
 
