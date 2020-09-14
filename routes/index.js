32f7405c5577f6d91597050a41babbfd128e9a61
Mohamed Mansour <mohamed28mansour@gmail.com>
2020-09-14 16:39:11 +0200

    add middleware and update index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router  = express.Router();
+const {loginCheck} = require('./middleware')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', {user: req.user});
 });
+
+router.get('/dashboard', loginCheck(), (req, res) => {
+  res.render('dashboard');
+})
 
 module.exports = router;
