d19a19b887743d26eec00c470b753843be913713
Habid B Espinosa <hab.bad@hotmail.com>
2020-09-15 14:02:16 +0200

    trainee buttons and javascript DOM manipulation added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+
+document.querySelector("#program-3-days").addEventListener('click', function(event){
+
+})
+
+document.querySelector('#program-6-days').addEventListener('click', function(event){
+
+})
