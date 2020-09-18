bee3f51f984ad6c255b964764e3cfc1f2fd7e0a4
Mohamed Mansour <mohamed28mansour@gmail.com>
2020-09-18 10:08:42 +0200

    update mongoose connection for deployment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const flash        = require('connect-flash')
 
 
 mongoose
-  .connect('mongodb://localhost/secondproject', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/secondproject', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
